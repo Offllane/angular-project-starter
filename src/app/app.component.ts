fee2b573bfb26e8d216b9d67859dbb7416be9159
Offllane <offllane@mail.ru>
2024-10-03 13:12:08 +0300

    setup basic routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HomePageComponent} from './features/pages/home-page/home-page.component';
+import {HeaderComponent} from './core/components/header/header.component';
+import {FooterComponent} from './core/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomePageComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
